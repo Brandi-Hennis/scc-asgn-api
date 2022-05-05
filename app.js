a4589e649b3a07d2df24af2f8611a341206f3405
Brandi-Hennis <102564951+Brandi-Hennis@users.noreply.github.com>
2022-05-05 12:50:55 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+/**
+ * app.js
+ * Entry point file for scc-asgn-api application
+ * Prog3
+ * Brandi Hennis
+ * COMP 2150 Web services
+ */
+ 
+let express = require('express');
+let app = express();
+const asgnRouter = require('./asgn-router.js');
+let mongoose = require("mongoose");
+let bodyParser = require("body-parser");
+
+//set up mongoose and body parser
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
+//connect to mongoose
+mongoose.connect("mongodb://localhost/assignment", { useNewUrlParser: true});
+var db = mongoose.connection;
+
+if(!db) {
+    console.log("Error connecting to the DB.");
+} else {
+    console.log("DB connected successfully!");
+}
+
+var port = 8080;
+
+app.use("/api", asgnRouter);
+
+app.get('/', (req, res) => res.send('Hello World with Express'));
+
+app.listen(port, function () {
+    console.info("Running scc-asgn-api on port " + port);
+});
